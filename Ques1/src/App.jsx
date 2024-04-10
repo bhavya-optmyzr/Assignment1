af1a8aad476e45af96e55c5ce95de3e6b8e84186
Bhavya Garg <bhavya@optmyzr.com>
2024-04-10 11:18:41 +0530

    some changesZ

--- Ques1/src/App.jsx
+++ Ques1/src/App.jsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1>Weather Api</h1>
-  )
+  return <h1>Weather Api</h1>;
 }
 
 export default App;
--- Ques1/src/api.js
+++ Ques1/src/api.js
@@ -1,51 +1,55 @@
-
 var MyModel = Backbone.Model.extend(); // Define a model (optional)
 
 var MyCollection = Backbone.Collection.extend({
-    model: MyModel,
-    initialize: function (options) {
-        this.latitude = options.latitude;
-        this.longitude = options.longitude;
-    },
-    url: function () {
-        return 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cddbf0b42be47fe6061841ce93bf9ab6';
-        // return 'https://api.openweathermap.org/data/3.0/onecall?lat=' + this.latitude + '&lon=' + this.longitude + '&exclude=daily&appid=cddbf0b42be47fe6061841ce93bf9ab6';
-    }
+  model: MyModel,
+  initialize: function (options) {
+    this.latitude = options.latitude;
+    this.longitude = options.longitude;
+  },
+  url: function () {
+    return "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cddbf0b42be47fe6061841ce93bf9ab6";
+    // return 'https://api.openweathermap.org/data/3.0/onecall?lat=' + this.latitude + '&lon=' + this.longitude + '&exclude=daily&appid=cddbf0b42be47fe6061841ce93bf9ab6';
+  },
 });
 
 var MyView = Backbone.View.extend({
-    el: '#root',
-    initialize: function () {
-        this.render();
-    },
-    render: function () {
-        this.$el.html("Loading...");
-        this.fetchData();
-    },
-    fetchData: function () {
-        var self = this;
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
+  el: "#root",
+  initialize: function () {
+    this.render();
+  },
+  render: function () {
+    this.$el.html("Loading...");
+    this.fetchData();
+  },
+  fetchData: function () {
+    var self = this;
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
 
-            var myData = new MyCollection({
-                latitude: latitude,
-                longitude: longitude
-            });
-
-            myData.fetch({
-                success: function (collection, response) {
-                    console.log(response);
-                    self.$el.html('Data fetched successfully: ' + JSON.stringify(response));
-                },
-                error: function (collection, response) {
-                    self.$el.html('Error fetching data: ' + JSON.stringify(response));
-                }
-            });
-        }, function (error) {
-            self.$el.html('Error getting user location: ' + JSON.stringify(error));
+        var myData = new MyCollection({
+          latitude: latitude,
+          longitude: longitude,
         });
-    }
+
+        myData.fetch({
+          success: function (collection, response) {
+            console.log(response);
+            self.$el.html(
+              "Data fetched successfully: " + JSON.stringify(response)
+            );
+          },
+          error: function (collection, response) {
+            self.$el.html("Error fetching data: " + JSON.stringify(response));
+          },
+        });
+      },
+      function (error) {
+        self.$el.html("Error getting user location: " + JSON.stringify(error));
+      }
+    );
+  },
 });
 
 var myView = new MyView();
