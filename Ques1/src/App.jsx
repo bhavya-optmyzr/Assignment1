c2e710f6fa7ae35d17c8249c4a547bf818ee18f8
Bhavya Garg <bhavya@optmyzr.com>
2024-04-10 17:24:13 +0530

    Started rendering more components

--- Ques1/src/App.jsx
+++ Ques1/src/App.jsx
@@ -1,5 +1,5 @@
 import CardStructure from "./components/CardStructure";
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import { Context } from "./Api";
 
 export const thisCity = "London,uk";
@@ -7,13 +7,24 @@ export const thisCity = "London,uk";
 export default function App() {
   const {city}=useContext(Context);
   const {weatherMain}=useContext(Context);
+
+  const [cards, setCards]=useState([""]);
+
+  function handleClick(input){
+    setCards(prev => {
+      return [...prev, input];
+    })
+  }
  
   return (
     <>
       <h1>Weather App</h1>
       
       <div id="cards">
-        <CardStructure city={city} weatherMain={weatherMain}/>
+        {cards.map((data, index) => {
+          return <CardStructure key={index} city={data} weatherMain={weatherMain} onClick={handleClick}/>
+        })}
+        
         {city}
       </div>
 
--- Ques1/src/components/CardStructure.jsx
+++ Ques1/src/components/CardStructure.jsx
@@ -1,10 +1,15 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import DayData from "./DayData";
+import {useState} from 'react';
 
-function CardStructure({ city , weatherMain}) {
+function CardStructure({ city , weatherMain , onClick}) {
+
+  const [input, setInput] = useState("");
+
   return (
     <Card>
+      <input value={input} required onChange = {(event) => {setInput(event.target.value)}}/>
       <Card.Header as="h5">{city}</Card.Header>
       <Card.Body>
         <Card.Title>Mainly : {weatherMain}</Card.Title>
@@ -19,7 +24,7 @@ function CardStructure({ city , weatherMain}) {
             <DayData/>
           </div>
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" onClick={()=>onClick(input)}>Add Location</Button>
       </Card.Body>
     </Card>
   );
