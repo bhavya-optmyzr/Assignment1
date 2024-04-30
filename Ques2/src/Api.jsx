c8ffa89a709258c3a193562dd055fb68a83de3ce
Bhavya Garg <bhavya@optmyzr.com>
2024-04-30 16:10:59 +0530

    Resolved the bug, and changed the asnyc function

--- Ques2/src/Api.jsx
+++ Ques2/src/Api.jsx
@@ -1,21 +1,20 @@
 const API_ID = "cddbf0b42be47fe6061841ce93bf9ab6";
 
-export async function fetchData(cardCity, setData) {
+export function fetchData(cardCity, setData) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${cardCity}&APPID=${API_ID}`;
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw "This location is not defined...Try another...";
-    }
-
-    const data = await response.json();
-    console.log(data);
-    setData({
-      temp: (data.main.temp - 273.15).toFixed(2),
-      city: data.name,
-      feelsLike: (data.main.feels_like - 273.15).toFixed(2),
-      desc: data.weather[0].description,
-    });
+    const response = fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData({
+          temp: (data.main.temp - 273.15).toFixed(2),
+          city: data.name,
+          feelsLike: (data.main.feels_like - 273.15).toFixed(2),
+          desc: data.weather[0].description,
+        });
+      });
   } catch (err) {
     alert(err);
   }
--- Ques2/src/App.jsx
+++ Ques2/src/App.jsx
@@ -6,10 +6,12 @@ export default function App() {
   const [city, setCity] = useState("London,uk");
   const [input, setInput] = useState("");
   const [cards, setCards] = useState([]);
+  const [prevId, setPrevId] = useState(0);
 
   function handleClick() {
     setCity(input);
-    const id = cards.length + 1;
+    const id = prevId + 1;
+    setPrevId(id);
     setCards((prev) => {
       return [...prev, { id: id, city: input }];
     });
@@ -38,8 +40,9 @@ export default function App() {
       <div id="cards">
         {cards.map((data, index) => {
           return (
+            // earlier i was using key as index which was causing error I have changed it to data's index which has resolved the bug.
             <CardStructure
-              key={index}
+              key={data.id}
               city={data.city}
               identifier={data.id}
               onDelete={handleDelete}
