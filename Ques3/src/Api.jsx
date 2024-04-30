3cd22dd44d543b8201ea0e1d471766f6ffdbc4ea
Bhavya Garg <bhavya@optmyzr.com>
2024-04-30 16:29:22 +0530

    corrected ques 3

--- Ques3/src/Api.jsx
+++ Ques3/src/Api.jsx
@@ -1,21 +1,20 @@
 const API_ID = "cddbf0b42be47fe6061841ce93bf9ab6";
 
-export async function fetchData(cardCity, setData) {
+export function fetchData(cardCity, setData) {
   let url = `https://localhost:7148/WeatherInfo/${cardCity}`;
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw "This location is not defined...Try another...";
-    }
-
-    const data = await response.json();
-    console.log(data);
-    setData({
-      temp: (data.main.temp).toFixed(2),
-      city: data.name,
-      feelsLike: (data.main.feels_like).toFixed(2),
-      desc: data.weather[0].description,
-    });
+    const response = fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData({
+          temp: (data.main.temp - 273.15).toFixed(2),
+          city: data.name,
+          feelsLike: (data.main.feels_like - 273.15).toFixed(2),
+          desc: data.weather[0].description,
+        });
+      });
   } catch (err) {
     alert(err);
   }
